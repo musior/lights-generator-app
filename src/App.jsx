44b6e95b47b3225642513b4caa9172859bcc3cec
Mateusz <108770502+musior@users.noreply.github.com>
2023-09-24 17:18:07 +0200

    feat: create dynamically grid lights templete generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,65 @@
+import { useState } from "react";
 import "./App.css";
 import { Counter } from "./components/Counter";
 import { SingleLight } from "./components/SingleLight";
 import { Text } from "./components/Text";
+import { Grid } from "./components/Grid";
 
 function App() {
+  const [columns, setColumns] = useState(0);
+  const [rows, setRows] = useState(0);
+
+  const handleIncrementRow = () => {
+    setRows(rows + 1);
+  };
+
+  const handleDecrementRow = () => {
+    if (rows <= 0) {
+      return;
+    }
+    setRows(rows - 1);
+  };
+
+  const handleIncrementColumns = () => {
+    if (columns >= 9) {
+      return;
+    }
+    setColumns(columns + 1);
+  };
+
+  const handleDecrementColumns = () => {
+    if (columns <= 0) {
+      return;
+    }
+    setColumns(columns - 1);
+  };
+
+  const totalLights = rows * columns;
+
+  const lights = Array.from({ length: totalLights });
+
   return (
     <div>
       <nav className="nav">
-        <Text>0 bulbs</Text>
-        <Counter text={"colums"} count={0} />
-        <Counter text={"rows"} count={0} />
+        <Text>{totalLights} bulbs</Text>
+        <Counter
+          text={"columns"}
+          count={columns}
+          onIncrement={handleIncrementColumns}
+          onDecrement={handleDecrementColumns}
+        />
+        <Counter
+          text={"rows"}
+          count={rows}
+          onIncrement={handleIncrementRow}
+          onDecrement={handleDecrementRow}
+        />
       </nav>
-      <div>
-        <SingleLight />
-      </div>
+      <Grid columns={columns}>
+        {lights.map((value, index) => {
+          return <SingleLight key={index} />;
+        })}
+      </Grid>
     </div>
   );
 }
